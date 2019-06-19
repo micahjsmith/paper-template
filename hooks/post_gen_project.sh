cdb3a94f15d1ce823e8a70b00cd7b9301295fe34
Micah Smith <micahjsmith@gmail.com>
2019-06-19 09:13:26 -0400

    fix cookiecutter

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-git config user.name "{{ cookiecutter.full_name }}"
-git config user.email "{{ cookiecutter.email }}"
-git remote add origin "https://git.overleaf.com/{{ cookiecutter.overleaf_id }}"
+git init
 git add .
 git commit -m "Automatically generated files from micahjsmith/paper-template"
+git remote add origin "https://git.overleaf.com/{{ cookiecutter.overleaf_id }}"
