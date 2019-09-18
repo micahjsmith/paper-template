90275c38315cd07bdea84715f959645e192be053
Micah Smith <micahjsmith@gmail.com>
2019-09-18 10:34:38 -0400

    fix merge

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,10 +4,10 @@ git init
 git add .
 git commit -m "Automatically generated files from micahjsmith/paper-template"
 git remote add origin "https://git.overleaf.com/{{ cookiecutter.overleaf_id }}"
-git branch --set-upstream-to=origin/master master
 
 # "Merge" remote
 if [[ -n "{{ cookiecutter.overleaf_id }}" ]]; then
     git fetch origin master
-    git merge --allow-unrelated-histories -s ours origin/master
+    git branch --set-upstream-to=origin/master master
+    git merge --allow-unrelated-histories -s ours --no-edit origin/master
 fi
