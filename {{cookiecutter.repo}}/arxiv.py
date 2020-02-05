14672fb6d9288cb4f5f69762693088186b293135
Micah Smith <micahjsmith@gmail.com>
2020-02-05 16:12:37 -0500

    Update arxiv.py

--- {{cookiecutter.repo}}/arxiv.py
+++ {{cookiecutter.repo}}/arxiv.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-from textwrap import dedent
 import pathlib
 import re
 import shutil
 import sys
+from textwrap import dedent
 
 import ply.lex
 
 def get_deps_file():
-    # TODO
+    # TODO recover actual builddir
     with open('./latexmkrc', 'r') as f:
         lines = f.readlines()
     for line in lines:
@@ -18,62 +18,13 @@ def get_deps_file():
 
     return pathlib.Path('.', 'build', 'deps')
 
+
 def remove_trailing_backslash(s):
     if s[-1] == '\\':
         return s[:-1]
     else:
         return s
 
-# make arxiv dir
-arxivdir = pathlib.Path('.', 'arxiv')
-
-# open deps file and find matches
-depsfile = get_deps_file()
-with depsfile.open('r') as f:
-    # skip two lines
-    for _ in range(2):
-        f.readline()
-
-    # now skip until path doesn't start with leading /
-    while True:
-        line = f.readline()
-        line = line.strip()
-        if line[0] != '/':
-            break
-
-    deps = [remove_trailing_backslash(line)]
-
-    # now read until #===End
-    while True:
-        line = f.readline()
-        line = line.strip()
-        if line.startswith('#===End'):
-            break
-        else:
-            deps.append(remove_trailing_backslash(line))
-
-
-assert deps
-
-
-# copy matches to new subdirectory
-for dep in deps:
-    src = pathlib.Path(dep)
-    dst = arxivdir.joinpath(dep)
-    dst.parent.mkdir(parents=True, exist_ok=True)
-    shutil.copyfile(src, dst)
-
-# create makefile
-with arxivdir.joinpath('Makefile').open('w') as f:
-    f.write(dedent('''\
-    .PHONY: main
-    main:
-    \tlatexmk -shell-escape -bibtex -pdf main
-
-    .PHONY: clean
-    clean:
-    \tlatexmk -C main
-    '''))
 
 def strip_comments(source):
     """Strip comments from LaTeX source files
@@ -200,16 +151,74 @@ def strip_comments(source):
     lexer.input(source)
     return u"".join([tok.value for tok in lexer])
 
-# remove comments from all files
-for dep in deps:
-    p = arxivdir.joinpath(dep)
-    if p.suffix != '.tex':
-        print(f'Skipping {p}')
-        continue
 
-    print(f'Stripping comments from {p}...')
-    with p.open('r') as f:
-        content = f.read()
-    content = strip_comments(content)
-    with p.open('w') as f:
-        f.write(content)
+def main():
+
+    # make arxiv dir
+    arxivdir = pathlib.Path('.', 'arxiv')
+
+    # open deps file and find matches
+    depsfile = get_deps_file()
+    with depsfile.open('r') as f:
+        # skip two lines
+        for _ in range(2):
+            f.readline()
+
+        # now skip until path doesn't start with leading /
+        while True:
+            line = f.readline()
+            line = line.strip()
+            if line[0] != '/':
+                break
+
+        deps = [remove_trailing_backslash(line)]
+
+        # now read until #===End
+        while True:
+            line = f.readline()
+            line = line.strip()
+            if line.startswith('#===End'):
+                break
+            else:
+                deps.append(remove_trailing_backslash(line))
+
+
+    assert deps
+
+
+    # copy matches to new subdirectory
+    for dep in deps:
+        src = pathlib.Path(dep)
+        dst = arxivdir.joinpath(dep)
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(src, dst)
+
+    # create makefile
+    with arxivdir.joinpath('Makefile').open('w') as f:
+        f.write(dedent('''\
+            .PHONY: main
+            main:
+            \tlatexmk -shell-escape -bibtex -pdf main
+
+            .PHONY: clean
+            clean:
+            \tlatexmk -C main
+        '''))
+
+    # remove comments from all files
+    for dep in deps:
+        p = arxivdir.joinpath(dep)
+        if p.suffix != '.tex':
+            print(f'Skipping {p}')
+            continue
+
+        print(f'Stripping comments from {p}...')
+        with p.open('r') as f:
+            content = f.read()
+        content = strip_comments(content)
+        with p.open('w') as f:
+            f.write(content)
+
+
+if __name__ == '__main__':
+    main()
